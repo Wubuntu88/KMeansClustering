275524f10fed4ed21ef8a4cd8fb4622c089ce6f0
William Gillespie <wgilles1@emich.du>
2016-04-05 19:16:18 -0400

    Added the Record class.

---
+++ src/Record.java
@@ -0,0 +1,25 @@
+public class Record {
+
+	private double[] attrList;
+	public double[] getAttrList() {
+		return attrList;
+	}
+
+	public Record(double[] attrList) {
+		this.attrList = attrList;
+	}
+	
+	public int numberOfAttributes(){
+		return attrList.length;
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer sBuffer = new StringBuffer("");
+		for (double dub : this.attrList) {
+			sBuffer.append(String.format("%.2f", dub) + ", ");
+		}
+		sBuffer.replace(sBuffer.length() - 2, sBuffer.length(), "");
+		return sBuffer.toString();
+	}
+}
