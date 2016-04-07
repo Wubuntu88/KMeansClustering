fec5af53bd708cb9879d86c7b401bce70c7e21d3
William Gillespie <wgilles1@emich.edu>
2016-04-07 11:54:01 -0400

    More testing, writing of the files.

--- src/Driver1.java
+++ src/Driver1.java
@@ -2,18 +2,21 @@
 public class Driver1 {
 
 	public static void main(String[] args) {
+		String inputFile = "Archive/file1";
+		String outputFile = "output1.txt";
+		
 		KMeansIO kmIO = new KMeansIO();
 		KMeansClusterer kmc = null;
 		try {
-			kmc = kmIO.instantiateKMeansClusterer("Archive/file1");
+			kmc = kmIO.instantiateKMeansClusterer(inputFile);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		//System.out.println(kmc.toString());
-		kmc.setParameters(4, 92378);
+		kmc.setParameters(5, 92378);
 		kmc.cluster();
-		System.out.println(kmc);
+		kmc.writeClusteredRecordsToFile(outputFile);
 	}
 
 }
