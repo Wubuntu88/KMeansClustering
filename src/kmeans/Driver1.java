33e208b6a12dbbda989aacbdf5092740785640e9
William Edward Gillespie <wgilles1@emich.edu>
2016-04-25 22:51:19 -0400

    Changed so that the change in the KMeansIO is reflected in the drivers.

--- src/kmeans/Driver1.java
+++ src/kmeans/Driver1.java
@@ -1,32 +1,34 @@
 package kmeans;
-import java.util.ArrayList;
+
 public class Driver1 {
-	public static void main(String[] args) throws Exception{
-		String inputFile = "Archive/file1";
-		String outputFile = "outputs/output1.txt";
-		
-		final int NUMBER_OF_CENTROIDS = 3;
-		KMeansIO kmIO = new KMeansIO();
-		KMeansClusterer kmc = kmIO.instantiateKMeansClusterer(inputFile);
-		kmc.setParameters(NUMBER_OF_CENTROIDS, 92378);
-		kmc.cluster();
-		/*
-		 * part B: centroid at end of clustering
-		 */
-		String centroidDescription = kmc.centroidString();
-		System.out.println("normalized centroids at end of clustering:");
-		System.out.println(centroidDescription);
-		/*
-		 * part C: sum squared error
-		 */
-		System.out.println(String.format("Clustering records with %s clusters.", kmc.getNumberOfClusters()));
-		System.out.println(String.format("sum squared error: %f", kmc.sumSquaredError()));
-		/*
-		 * Writing summary to file (part A)
-		 */
-		ArrayList<Record> denormalizedRecords = kmc.recordsCopy();
-		int[] clustersRecordsBelongTo = kmc.clustersThatRecordsBelongToCopy();
-		int numClusters = kmc.getNumberOfClusters();
-		kmIO.writeClusteredRecordsToFile(outputFile, denormalizedRecords, clustersRecordsBelongTo, numClusters);
-	}
+    public static void main(String[] args) throws Exception {
+	String inputFile = "Archive/file1";
+	String outputFile = "outputs/output1.txt";
+
+	final int NUMBER_OF_CENTROIDS = 8;
+	KMeansIO kmIO = new KMeansIO(inputFile);
+	KMeansClusterer kmc = kmIO.instantiateKMeansClusterer();
+	kmc.setParameters(NUMBER_OF_CENTROIDS, 92378);
+	kmc.cluster();
+	/*
+	 * part B: centroid at end of clustering
+	 */
+	String centroidDescription = kmc.centroidString();
+	System.out.println("normalized centroids at end of clustering:");
+	System.out.println(centroidDescription);
+	/*
+	 * part C: sum squared error
+	 */
+	System.out.println(String.format("Clustering records with %s clusters.", kmc.getNumberOfClusters()));
+	System.out.println(String.format("sum squared error: %f", kmc.sumSquaredError()));
+	/*
+	 * Writing summary to file (part A)
+	 */
+	// ArrayList<Record> denormalizedRecords = kmc.recordsCopy();
+	// int[] clustersRecordsBelongTo =
+	// kmc.clustersThatRecordsBelongToCopy();
+	// int numClusters = kmc.getNumberOfClusters();
+	// kmIO.writeClusteredRecordsToFile(outputFile, denormalizedRecords,
+	// clustersRecordsBelongTo, numClusters);
+    }
 }
--- src/kmeans/Driver2.java
+++ src/kmeans/Driver2.java
@@ -1,26 +1,28 @@
 package kmeans;
+
 import java.util.ArrayList;
+
 public class Driver2 {
-	public static void main(String[] args) throws Exception {
-		String inputFile = "Archive/file2";
-		String outputFile = "outputs/output2.txt";
-		
-		final int NUMBER_OF_CENTROIDS = 4;
-		KMeansIO kmIO = new KMeansIO();
-		KMeansClusterer kmc = kmIO.instantiateKMeansClusterer(inputFile);
-		kmc.setParameters(NUMBER_OF_CENTROIDS, 92378);
-		kmc.cluster();
-		/*
-		 * part C: sum squared error
-		 */
-		System.out.println(String.format("Clustering records with %s clusters.", kmc.getNumberOfClusters()));
-		System.out.println(String.format("sum squared error: %f", kmc.sumSquaredError()));
-		/*
-		 * Writing summary to file (part A)
-		 */
-		ArrayList<Record> denormalizedRecords = kmc.recordsCopy();
-		int[] clustersRecordsBelongTo = kmc.clustersThatRecordsBelongToCopy();
-		int numClusters = kmc.getNumberOfClusters();
-		kmIO.writeClusteredRecordsToFile(outputFile, denormalizedRecords, clustersRecordsBelongTo, numClusters);
-	}
+    public static void main(String[] args) throws Exception {
+	String inputFile = "Archive/file2";
+	String outputFile = "outputs/output2.txt";
+
+	final int NUMBER_OF_CENTROIDS = 4;
+	KMeansIO kmIO = new KMeansIO(inputFile);
+	KMeansClusterer kmc = kmIO.instantiateKMeansClusterer();
+	kmc.setParameters(NUMBER_OF_CENTROIDS, 92378);
+	kmc.cluster();
+	/*
+	 * part C: sum squared error
+	 */
+	System.out.println(String.format("Clustering records with %s clusters.", kmc.getNumberOfClusters()));
+	System.out.println(String.format("sum squared error: %f", kmc.sumSquaredError()));
+	/*
+	 * Writing summary to file (part A)
+	 */
+	ArrayList<Record> denormalizedRecords = kmc.recordsCopy();
+	int[] clustersRecordsBelongTo = kmc.clustersThatRecordsBelongToCopy();
+	int numClusters = kmc.getNumberOfClusters();
+	kmIO.writeClusteredRecordsToFile(outputFile, denormalizedRecords, clustersRecordsBelongTo, numClusters);
+    }
 }
